6e81c1a64d5bad3f183bf687a17eb3f7e3f7c94e
Ravi <vvpravikoundinyabhavans@gmail.com>
2022-02-21 16:33:15 +0530

    classes

--- shop.py
+++ shop.py
@@ -1,1 +1,14 @@
-print("hello world")
+class customer:
+    def __init__(self,name,gender,age,items):
+        self.name = name
+        self.gender = gender
+        self.age = age
+        self.items = items
+        self.status = False
+        self.type = "n"
+        self.total = 0
+
+class smart:
+    def __init__(self,clist):
+        smart.clist = clist
+ 
