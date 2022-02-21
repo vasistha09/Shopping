8ad94e4dee79fe7b150df7615dab907091c9b811
Ravi <vvpravikoundinyabhavans@gmail.com>
2022-02-21 17:21:47 +0530

    final

--- shop.py
+++ shop.py
@@ -1,17 +1,47 @@
 
+from pickle import TRUE
+
+
 class customer:
     def __init__(self,name,gender,age,items):
         self.name = name
         self.gender = gender
         self.age = age
         self.items = items
-        self.status = False
+        self.status = "False"
         self.type = "n"
         self.total = 0
 
 class smart:
     def __init__(self,cust):
         smart.cust = cust
+
+def check(cust):
+    for c in cust:
+        sum =0
+        for p in c.items:
+            sum = sum+c.items[p]
+        c.total = sum
+        if(c.age>=50 and sum>=500 ):
+            c.type = "f"
+
+def disc(cust):
+    for c in cust:
+        if (c.type=="f" and c.total<=1000 ):
+            c.total = c.total - c.total*0.1
+            c.status = "true"
+        elif(c.total>1000 and c.total<2500):
+            c.total = c.total - c.total*0.15
+            c.status = "true"
+        elif(c.total>=2500):
+            c.total = c.total - c.total*0.25
+            c.status = "true"
+        print("{} total amount = {} discount status {}".format(c.name,c.total,c.status))
+
+        
+
+    
+
 
 
 
@@ -28,5 +58,10 @@ if __name__=="__main__":
             item = input()
             price = int(input())
             products.update({item:price})
-        print(products)
+        cust.append(customer(name,gender,age,products))
+    check(cust)
+    disc(cust)
+    
+    
+
 
