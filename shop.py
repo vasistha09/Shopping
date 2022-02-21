b847521d54417749cb82aaaea7cdf1b1cb8ab34b
GopalVasistha <vgopalv99@gmail.com>
2022-02-21 16:30:14 +0530

    sexy

--- shop.py
+++ shop.py
@@ -1,1 +1,16 @@
-print("hello world")
+
+
+if __name__=="__main__":
+    cust = []
+    no_of_cust = int(input())
+    for i in range(no_of_cust):
+        name = input()
+        gender = input()
+        age = int(input())
+        n_items = int(input())
+        products = {}
+        for i in range(n_items):
+            item = input()
+            price = int(input())
+            products.update({item:price})
+        print(products)
